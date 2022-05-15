9c1b3507e392a53f56ba7529f7404ff9ada4a0d6
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-05-14 21:51:24 -0300

    Course (Lecture #9): Color Literal, Gradient and Overlay

--- swiftui-designcode-wallet/swiftui-designcode-wallet/Scene/Menu/View/MenuView.swift
+++ swiftui-designcode-wallet/swiftui-designcode-wallet/Scene/Menu/View/MenuView.swift
@@ -13,16 +13,43 @@ struct MenuView: View {
             Spacer()
             
             VStack(spacing: 16) {
+                Text("Meng - 28% complete")
+                    .font(.caption)
+                
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .clipShape(Capsule())
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(.black.opacity(0.08))
+                    .clipShape(Capsule())
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(.black.opacity(0.1))
+                    .clipShape(Capsule())
+                
                 MenuRow(systemName: "gear", titleRow: "Account")
                 MenuRow(systemName: "creditcard", titleRow: "billing")
                 MenuRow(systemName: "person.crop.circle", titleRow: "Sign out")
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
-            .background(.white)
+            .background(
+                LinearGradient(
+                    colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8507653061, green: 0.8853369473, blue: 0.8673735119, alpha: 1))],
+                    startPoint: .top, endPoint: .bottom
+                )
+            )
             .clipShape(RoundedRectangle.init(cornerRadius: 20, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: .black.opacity(0.2), radius: 20, x: 0, y: 20)
             .padding(.horizontal, 30)
+            .overlay(
+                Image("Avatar")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
         }
         .padding(.bottom, 30)
     }
