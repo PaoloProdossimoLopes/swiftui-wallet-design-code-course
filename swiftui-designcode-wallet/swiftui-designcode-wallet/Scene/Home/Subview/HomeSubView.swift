eadc0b42d889e7e069323a8f1ad219cdfe31fae4
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-05-15 09:55:32 -0300

    Course 1 (Lecture #13): ScrollView and Repeat

--- swiftui-designcode-wallet/swiftui-designcode-wallet/Scene/Home/Subview/HomeSubView.swift
+++ swiftui-designcode-wallet/swiftui-designcode-wallet/Scene/Home/Subview/HomeSubView.swift
@@ -24,6 +24,17 @@ struct HomeSubView: View {
             .padding(.horizontal)
             .padding(.top, 30)
             
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(0..<5) { _ in
+                        SectionView()
+                    }
+                }
+                .padding(.leading)
+            }
+            .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
+            
+            
             Spacer()
         }
     }
@@ -32,5 +43,36 @@ struct HomeSubView: View {
 struct HomeSubView_Previews: PreviewProvider {
     static var previews: some View {
         HomeSubView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Prototype design in swiftUI")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                Image("Logo1")
+            }
+            
+            Text("18 Section".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("Card1")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 210)
+            
+        }
+        .padding([.top, .horizontal], 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+//        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
     }
 }
